eddc05c6d6de880e80a75e19e0b0bcd749ee1e5d
Cyril Kato <contact@cyril.email>
2023-07-09 16:45:50 +0200

    feat: new version

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,26 @@ self.addEventListener('install', (event) => {
     caches.open('v1').then((cache) => {
       return cache.addAll([
         '/',
-        '/img/komayo-in-ogi-game-with-victim.webp',
+        '/css/pico.classless.min.css',
+        '/favicon-512x512.png',
+        '/favicon.svg',
+        '/manifest.json',
+        '/komayo-souriant-au-dessus-de-l-adversaire-endormi.png',
+        '/komayo-souriant-au-dessus-de-l-adversaire-endormi.webp',
+        '/la-partie-inachevee/',
+            '/la-partie-inachevee/travail-prolonge-apres-heure-reglementaire.webp',
+            '/la-partie-inachevee/depart-brusque-bureau-atmosphere-confinee.webp',
+            '/la-partie-inachevee/paysage-nocturne-osaka-lumieres-astres.webp',
+            '/la-partie-inachevee/parties-shogi-bar-regence-soir.webp',
+            '/la-partie-inachevee/femme-kimono-bleu-bar-regence.webp',
+            '/la-partie-inachevee/plateau-ogi-pieces-configurations.webp',
+            '/la-partie-inachevee/mysterieuse-adversaire-presente-princesse-ogi.webp',
+            '/la-partie-inachevee/duel-ogi-determination-defi.webp',
+            '/la-partie-inachevee/miroirs-clartelunaire-adversaire-ogi.webp',
+            '/la-partie-inachevee/sourire-triomphant-lune-lointaine.webp',
+            '/la-partie-inachevee/temps-evapore-sommeil-plateau-jeu.webp',
+            '/la-partie-inachevee/piece-silencieuse-feuille-papier.webp',
+            '/la-partie-inachevee/deambulation-rues-desertes-melancolie-espoir.webp',
       ]);
     })
   );
